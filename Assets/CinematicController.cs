aca78d224b32f24e2d2abc6f630c94f1a103925f
James Gratrix <jamesgratrix@hotmail.co.uk>
2021-03-05 00:55:49 +0000

    Added new cinematic script, but we finish it tomoz Also hello future james

---
+++ Assets/CinematicController.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CinematicController : MonoBehaviour
+{
+    //We are going to want to have a person laying down, camera starts zoomed in on the eyeball.
+    //We can do a wierd anim of the eyeball rotating
+    //Cam zooms out more to see full face, we then have him smile (thumbs up also maybe?)
+    //Finally we then smooth transition to the gameplay camera, and everything starts
+}
